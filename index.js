e5268bbda10798790ba2a0b274d38eb69da6aefa
Martha Solomon <marthsolomon@gmail.com>
2021-12-05 12:30:20 -0500

    completed except check 1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+ const employee = {
+     name: "Sam",
+     streetAddress: "11 Broadway",
+ };
+
+ function updateEmployeeWithKeyAndValue (employee, name, streetAddress) {
+     const newEmploye = {...employee};
+     return newEmploye;
+ };
+
+ function destructivelyUpdateEmployeeWithKeyAndValue (employee, streetAddress, value) {
+     employee.streetAddress = "12 Broadway";
+     return employee;
+ };
+
+ function deleteFromEmployeeByKey (employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee.name
+    return newEmployee;
+ };
+
+ function destructivelyDeleteFromEmployeeByKey (employee, key) {
+     employee.name = undefined
+     return employee
+ };
